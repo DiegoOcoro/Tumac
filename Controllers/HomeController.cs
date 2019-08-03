ba3cb92fc946d78c1631cb23507f9762d24b9acb
Yura <miyura1990lg@hotmail.com>
2019-08-03 10:56:07 -0500

    primer sitio web

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Institucion_Educativa_Tumac.Models;
+
+namespace Institucion_Educativa_Tumac.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {           
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        
+        public IActionResult Instalacion()
+        {
+            return View();
+        }
+
+        public IActionResult Comedor()
+        {
+            return View();
+        }
+
+        public IActionResult Uniformes()
+        {
+            return View();
+        }
+
+        public IActionResult Ampa()
+        {
+            return View();
+        }
+
+        public IActionResult Horarios()
+        {
+            return View();
+        }
+
+        public IActionResult Act_Extraescolares()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Por_Tumac()
+        {
+            return View();
+        }
+
+        public IActionResult Modo_Manera()
+        {
+            return View();
+        }
+
+        public IActionResult Mision()
+        {
+            return View();
+        }
+
+        public IActionResult Historia()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
